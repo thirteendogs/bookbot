a4ef5b69a4d0f82151bb69b73446f19029f4813d
thirteendogs <daniel.nova.girbau@gmail.com>
2024-03-22 23:51:55 +0100

    feat: add count_characters function

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-def num_words_in_book(book):
-    num_words = len(book.split())
-    print(num_words)
-
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        num_words_in_book(file_contents)
+        book_words = get_book_words(file_contents)
+        num_words_in_book(book_words)
+        count_characters(book_words)
+
+def get_book_words(book):
+    return book.split()
+        
+def num_words_in_book(book_words):
+    print(len(book_words))
+
+def count_characters(book_words):
+    characters_count = {}
+    book_charaters = ''.join(book_words).lower()
+
+    for char in book_charaters:
+        if char not in characters_count:
+            characters_count[char] = 1
+        else:
+            characters_count[char] +=1
+    print(characters_count)
 
 main()
 
