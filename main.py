35aaaf1b2ab27fe37b5f88bae57a921e5a7c792f
thirteendogs <daniel.nova.girbau@gmail.com>
2024-03-23 00:14:07 +0100

    feat: clean non alpha characters from the list

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
+    print(f"--- Starting the report for {book} ---")
+    with open(book) as f:
         file_contents = f.read()
         book_words = get_book_words(file_contents)
-        num_words_in_book(book_words)
-        count_characters(book_words)
+        num_words_in_book = get_num_words_in_book(book_words)
+        print(f"This book has {num_words_in_book} words!")
+        get_count_characters_dict(book_words)
 
 def get_book_words(book):
     return book.split()
         
-def num_words_in_book(book_words):
-    print(len(book_words))
+def get_num_words_in_book(book_words):
+    return len(book_words)
 
-def count_characters(book_words):
-    characters_count = {}
+def get_count_characters_dict(book_words):
+    characters_count_dict = {}
     book_charaters = ''.join(book_words).lower()
 
     for char in book_charaters:
-        if char not in characters_count:
-            characters_count[char] = 1
-        else:
-            characters_count[char] +=1
-    print(characters_count)
+        if char.isalpha() == True:
+            if char not in characters_count_dict:
+                characters_count_dict[char] = 1
+            else:
+                characters_count_dict[char] +=1
+    print(characters_count_dict)
+    return characters_count_dict
+
+book = "./books/frankenstein.txt"
 
 main()
 
